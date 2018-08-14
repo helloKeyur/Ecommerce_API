f0704ee0f512c1b2bcafbdbe55ab2c8053ae4e0d
keyur <keyur@zillionweb.com>
2018-08-14 19:22:00 +0530

    factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\model\Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word,
+        'detail' => $faker->text(100),
+        'price' => $faker->numberBetween(100,1000),
+        'stock' => $faker->randomDigit,
+        'discount' => $faker->numberBetween(2,40)
     ];
 });
--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\model\Product;
 use Faker\Generator as Faker;
 
 $factory->define(App\model\Review::class, function (Faker $faker) {
     return [
-        //
+        'customer' => $faker->name,
+        'review' => $faker->text(200),
+        'star' => $faker->numberBetween(0,5),
+        'product_id' => function(){
+        	return Product::all()->random();
+        }
     ];
 });
